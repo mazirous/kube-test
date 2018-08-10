cf35413800af39f46b306f70ec6ef7c8fb6a4484
Apichart(BHT) <apichart.pnp@gmail.com>
2018-08-10 17:46:20 +0700

    no message

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -19,6 +19,7 @@ namespace WebApplication1
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://*:8080")
                 .UseStartup<Startup>();
     }
 }
